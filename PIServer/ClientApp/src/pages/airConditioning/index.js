587c1ee6f5243ca5ad22e59c5980fe0a2fe3e888
LzxHahaha <lzxglhf@live.com>
2019-04-16 20:07:25 +0800

    init server

---
+++ PIServer/ClientApp/src/pages/airConditioning/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class AirConditioning extends Component {
+    static displayName = AirConditioning.name;
+    
+    render() {
+        
+    }
+}
